bdb4009ef131c6f8da99f4d56964e383370c5afc
Neta Becker <netabec3@gmail.com>
2024-08-02 14:02:14 +0300

    The augmentation data is now exported to Excel file

--- create_augmentations.py
+++ create_augmentations.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import random
 
+percentage = 30
 # Path to your input TXT file
 input_txt_path = 'datasets/hebrew_text.txt'
 # Path to your output TXT file
-output_txt_path = 'datasets/hebrew_text_aug.txt'
-
+output_path = 'datasets/hebrew_text_aug_' + str(percentage)
+output_txt_path = output_path + '.txt'
 
 # Function to randomly replace letters
 def random_replace(string):
@@ -32,9 +33,8 @@ def random_replace(string):
     string_list = list(string)
     for idx, char in enumerate(string_list):
         if char in replacements:
-            if random.random() < 0.4:  # chance that a letter will be replaced
+            if (random.random() * 100) < percentage:  # chance that a letter will be replaced
                 string_list[idx] = random.choice(replacements[char])
-    # Convert list back to string
     return ''.join(string_list)
 
 
@@ -45,12 +45,23 @@ with open(input_txt_path, 'r', encoding='utf-8') as infile:
 # Process each line
 processed_lines = []
 for line in lines:
-    line = line.strip()  # Remove any leading/trailing whitespace
+    line = line.strip()
     modified_line = random_replace(line)
     processed_lines.append(f"{line}\t{modified_line}")
 
-# Write the original and modified text to the output TXT file
-with open(output_txt_path, 'w', encoding='utf-8') as outfile:
-    outfile.write('\n'.join(processed_lines))
+# Save data in txt format - uncomment to activate
+# # Write the original and modified text to the output TXT file
+# with open(output_txt_path, 'w', encoding='utf-8') as outfile:
+#     outfile.write('\n'.join(processed_lines))
+#
+# print(f"Modified data saved to {output_txt_path}")
 
-print(f"Modified data saved to {output_txt_path}")
+print(f'\nExporting the data to Excel file')
+
+processed_lines = processed_lines[1:]
+data = [line.strip().split('\t') for line in processed_lines]
+df = pd.DataFrame(data, columns=['original', 'errors'])  # Adjust column names as needed
+excel_output_path = output_path + '.xlsx'
+df.to_excel(excel_output_path, index=False, engine='openpyxl')
+
+print(f"Conversion complete. Check {excel_output_path}")
