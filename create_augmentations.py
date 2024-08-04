b72a79bd96a90e7e873b86852174be60445ec5da
Neta Becker <netabec3@gmail.com>
2024-08-04 16:04:59 +0300

    divided to designated functions

--- create_augmentations.py
+++ create_augmentations.py
@@ -1,38 +1,31 @@
 import pandas as pd
 import random
+import datasets as ds
+from datasets import Dataset
 
-verbose = True
-percentage = 30
-default_prob = float(percentage) / 100
-# Path to your input TXT file
-input_txt_path = 'datasets/hebrew_text.txt'
-# Path to your output TXT file
-output_path = 'datasets/hebrew_text_aug_' + str(percentage)
-output_txt_path = output_path + '.txt'
 
-# Function to randomly replace letters
-def random_replace(string):
+def random_replace(string, default_prob):
     replacements = {
-            'א': [('ע', default_prob), ('ה', default_prob)],
-            'ע': [('א', default_prob), ('ה', default_prob)],
-            'ה': [('א', default_prob), ('ע', default_prob)],
+        'א': [('ע', default_prob), ('ה', default_prob)],
+        'ע': [('א', default_prob), ('ה', default_prob)],
+        'ה': [('א', default_prob), ('ע', default_prob)],
 
-            'ט': [('ת', default_prob)],
-            'ת': [('ט', default_prob)],
+        'ט': [('ת', default_prob)],
+        'ת': [('ט', default_prob)],
 
-            'ח': [('כ', default_prob)],
-            'כ': [('ח', default_prob), ('ק', default_prob)],
-            'ק': [('כ', default_prob)],
+        'ח': [('כ', default_prob)],
+        'כ': [('ח', default_prob), ('ק', default_prob)],
+        'ק': [('כ', default_prob)],
 
-            'ש': [('ס', default_prob / 2)],
-            'ס': [('ש', default_prob / 2)],
+        'ש': [('ס', default_prob / 2)],
+        'ס': [('ש', default_prob / 2)],
 
-            'ב': [('ו', default_prob / 4)],
-            'ו': [('ב', default_prob / 4)],
+        'ב': [('ו', default_prob / 4)],
+        'ו': [('ב', default_prob / 4)],
 
-            'לא ': ('לו ', default_prob),
-            'לו ': [('לא ', default_prob)]
-        }
+        'לא ': ('לו ', default_prob),
+        'לו ': [('לא ', default_prob)]
+    }
 
     # Convert string to list to make replacements
     string_list = list(string)
@@ -45,35 +38,63 @@ def random_replace(string):
     return ''.join(string_list)
 
 
-# Read the input TXT file
-with open(input_txt_path, 'r', encoding='utf-8') as infile:
-    lines = infile.readlines()
+def create_augmentations(percentage=30, verbose=True):
+    default_prob = float(percentage) / 100
+    input_txt_path = 'datasets/hebrew_text.txt'
+    output_path = 'datasets/hebrew_text_aug_' + str(percentage)
 
-# Process each line
-processed_lines = []
-for line in lines:
-    line = line.strip()
-    modified_line = random_replace(line)
-    processed_lines.append(f"{line}\t{modified_line}")
+    # Read the input TXT file
+    with open(input_txt_path, 'r', encoding='utf-8') as infile:
+        lines = infile.readlines()
 
-if verbose:
-    print(f'-----------> Example:\n\n')
-    print(processed_lines[1])
-    print(f'<-----------= Example:\n\n')
+    # Process each line
+    processed_lines = []
+    for line in lines:
+        line = line.strip()
+        modified_line = random_replace(line, default_prob)
+        processed_lines.append(f"{line}\t{modified_line}")
 
-# Save data in txt format - uncomment to activate
-# # Write the original and modified text to the output TXT file
-# with open(output_txt_path, 'w', encoding='utf-8') as outfile:
-#     outfile.write('\n'.join(processed_lines))
-#
-# print(f"Modified data saved to {output_txt_path}")
+    if verbose:
+        print(f'-----------> Example:\n\n')
+        print(processed_lines[1])
+        print(f'<-----------= Example:\n\n')
 
-print(f'\nExporting the data to Excel file')
+    # Save data in txt format - uncomment to activate
+    # # Write the original and modified text to the output TXT file
+    # output_txt_path = output_path + '.txt'
+    # with open(output_txt_path, 'w', encoding='utf-8') as outfile:
+    #     outfile.write('\n'.join(processed_lines))
+    #
+    # print(f"Modified data saved to {output_txt_path}")
 
-processed_lines = processed_lines[1:]
-data = [line.strip().split('\t') for line in processed_lines]
-df = pd.DataFrame(data, columns=['original', 'errors'])  # Adjust column names as needed
-excel_output_path = output_path + '.xlsx'
-df.to_excel(excel_output_path, index=False, engine='openpyxl')
+    print(f'\nExporting the data to Excel file')
 
-print(f"Conversion complete. Check {excel_output_path}")
+    processed_lines = processed_lines[1:]
+    data = [line.strip().split('\t') for line in processed_lines]
+    df = pd.DataFrame(data, columns=['original', 'errors'])  # Adjust column names as needed
+    excel_output_path = output_path + '.xlsx'
+    df.to_excel(excel_output_path, index=False, engine='openpyxl')
+
+    print(f"Conversion complete. Check {excel_output_path}")
+    return excel_output_path
+
+
+def export_dataset(excel_path):
+    df = pd.read_excel(excel_path)
+    df.dropna(subset=['errors', 'original'], inplace=True)
+    texts_with_errors = df['errors'].tolist()
+    texts_corrected = df['original'].tolist()
+
+    data_dict = {
+        'errors': texts_with_errors,
+        'original': texts_corrected
+    }
+
+    dataset = ds.Dataset.from_dict(data_dict)
+    return dataset
+
+
+def full_run(percentage=30, verbose=True):
+    return export_dataset(create_augmentations(percentage, verbose))
+
+
